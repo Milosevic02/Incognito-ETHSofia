e43c8f074db2b8113af20e2bb19fa0a85f143d94
Dragan <milosevicdragan002@gmail.com>
2024-10-18 16:10:03 +0300

    Change folder

--- model/test.py
+++ model/test.py
@@ -1,19 +1,14 @@
 import pandas as pd
 import joblib
 
-# Učitaj K-means model
 kmeans = joblib.load('kmeans_model.pkl')
 
-# Funkcija za predviđanje klastera na osnovu novih podataka
 def predict_cluster(new_data):
-    # Pretvori nove podatke u DataFrame
     new_df = pd.DataFrame([new_data], columns=['balance', 'NumberOfTransaction', 'lastTransaction', 'firstTransaction', 'NFTCount', 'NativeBalance'])
     
-    # Predviđanje klastera
     cluster = kmeans.predict(new_df)
     return cluster[0]
 
-# Primer novih podataka
 new_user = {
     'balance': 100000,
     'NumberOfTransaction': 200,
@@ -23,9 +18,7 @@ new_user = {
     'NativeBalance': 10
 }
 
-# Predviđanje klastera
 predicted_cluster = predict_cluster(new_user)
 
-# Učitaj adrese iz predviđenog klastera
 addresses_in_cluster = pd.read_csv(f'cluster_{predicted_cluster}.csv')
 print(f"Addresses in Cluster {predicted_cluster}:\n", addresses_in_cluster)
